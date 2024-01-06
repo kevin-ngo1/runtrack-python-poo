cb3638240e28b7899d5d58de0b25384db2e88fc0
kevin-ngo1 <kevin.ngo@laplateforme.io>
2024-01-06 12:49:53 +0100

    Update Def Start JOB06

--- jour04/job06.py
+++ jour04/job06.py
@@ -8,6 +8,9 @@ class Vehicule():
     def informationsVehicule(self):
         print("Marque :",self.marque,"\nModèle :",self.modele,"\nAnnée :",self.annee,"\nPrix :",self.prix)
 
+    def demarrer(self):
+        print("Attention, je roule")
+
 class Voiture(Vehicule):
     def __init__(self,marque="",modele="",annee=0,prix=0,portes=4):
         Vehicule.__init__(self,marque,modele,annee,prix)
@@ -16,6 +19,9 @@ class Voiture(Vehicule):
     def informationsVehicule(self):
         Vehicule.informationsVehicule(self)
         print("Nombre de portes :",self.portes)
+
+    def demarrer(self):
+        print("La",self.marque,self.modele,"démarre ! C'est cars ou quoi")
 
 class Moto(Vehicule):
     def __init__(self,marque="",modele="",annee=0,prix=0,roue=2):
@@ -26,9 +32,14 @@ class Moto(Vehicule):
         Vehicule.informationsVehicule(self)
         print("Nombre de roues :",self.roue)
 
+    def demarrer(self):
+        print("La",self.marque,self.modele,"démarre ! VROUM-VROUM La moto du siècle")
 
 voiture1=Voiture("Mazda", "RX-7", 1996, 50000)
 moto1=Moto("Kawasaki","Ninja ZX-10R",2024,21000)
 voiture1.informationsVehicule()
 moto1.informationsVehicule()
+voiture1.demarrer()
+moto1.demarrer()
+
 
